4617d56f5999bacbdc0979c401ad29a7ba1162f1
mehrozezaidi <mehrozabbas14@gmail.com>
2025-09-29 11:17:59 +0500

    added updated android, ios and web directory

---
+++ android/app/src/main/kotlin/com/mehroo/quickflutterproject/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mehroo.quickflutterproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/menroo/flutter_template_with_riverpod/flutter_new_project_template_with_riverpod/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.menroo.flutter_template_with_riverpod.flutter_new_project_template_with_riverpod
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
